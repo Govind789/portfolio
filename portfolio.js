a825e5ee76c79c3e6c3234e1a5af49b2e8fa0dfa
Govind Gupta <gvgupta789@gmail.com>
2024-02-16 22:59:05 +0530

    menu-bar-update

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,29 @@
+
 function showContent(event){
     let Elm = event.target.getAttribute("data-id");
     console.log(event.target.classList)
     let targetElm = document.querySelector(`.${Elm}`);
     let activeElm = document.querySelector(".active-section");
+    let Tran = document.getElementById("check");
     if(activeElm){
         activeElm.classList.remove("active-section");
-        activeElm.classList.add("hidden")
+        activeElm.classList.add("hidden");
     }
     if(targetElm && targetElm.classList.contains("hidden")){
         targetElm.classList.remove("hidden");
         targetElm.classList.add("active-section");
+    }
+    if(Tran.checked){
+        Tran.checked = false;
+    }
+}
+
+function rotateBtn(){
+    var bar = document.querySelector(".box")
+    if(bar.classList.contains("myactive")){
+        bar.classList.remove("myactive");
+    }
+    else{
+        bar.classList.add("myactive");
     }
 }
