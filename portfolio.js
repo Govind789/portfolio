e20cf5da0523cdc6e7549edab19649c204894c6e
Govind Gupta <gvgupta789@gmail.com>
2024-06-26 23:45:56 +0530

    append issue

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,16 @@
 
 function showContent(event){
-    let bar = document.querySelector(".box");
+    var bar = document.querySelector(".box");
     let Elm = event.target.getAttribute("data-id");
     let targetElm = document.querySelector(`.${Elm}`);
     let activeElm = document.querySelector(".active-section");
     let Tran = document.getElementById("check");
-    if(activeElm && bar.classList.contains("myactive")){
+    if(activeElm){
         activeElm.classList.remove("active-section");
         activeElm.classList.add("hidden");
         bar.classList.remove("myactive");
     }
-    if(targetElm && targetElm.classList.contains("hidden")){
+    if(targetElm.classList.contains("hidden")){
         targetElm.classList.remove("hidden");
         targetElm.classList.add("active-section");
     }
